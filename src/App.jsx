d0b44f712e54f974453f85066cd5459e08958526
Qadeer Ahmad <qadeerahmad294gb@gmail.com>
2024-08-28 23:07:37 +0500

    add basic website templete & some components home,header,footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
 
 
 function App() {
@@ -7,7 +10,7 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-green-600 p-4'>React Router</h1>
+     
     </>
   )
 }
